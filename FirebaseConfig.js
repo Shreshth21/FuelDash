fbea0769e09613a7d090625b5a2b33202eccbbaa
Shreshth Verma <shreshthverma2001@gmail.com>
2024-07-05 01:11:08 +0530

    environment variables changes

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -6,16 +6,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 let firebaseApp = null;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXz0rxl3OEnIKfVR2OTj9mIxHTR7ULq_w",
-  authDomain: "fueldash-eba84.firebaseapp.com",
-  databaseURL: "https://fueldash-eba84-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fueldash-eba84",
-  storageBucket: "fueldash-eba84.appspot.com",
-  messagingSenderId: "371192371165",
-  appId: "1:371192371165:web:431f26572bfaebf9710834",
-  measurementId: "G-CGPT3CNZ4H"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
-
+console.log("api key", process.env.API_KEY);
 if (firebase.apps.length === 0) {
   firebaseApp = firebase.initializeApp(firebaseConfig);
 }
